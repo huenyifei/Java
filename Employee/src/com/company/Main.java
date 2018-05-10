4a75822eca5fd4d2e0ce0f79b043f54d3ffe7684
AAT.Admin.Colin <wuxinxin@zpmc.net>
2018-05-10 19:44:14 +0800

    Java核心技术 第5章 继承

--- Employee/src/com/company/Main.java
+++ Employee/src/com/company/Main.java
@@ -3,10 +3,37 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+//        /*
+//         * 命名Manager的内存空间比Employee的大
+//         * 但是却不可以存放
+//         */
+//        Manager[] manager = new Manager[3];
+//        Employee employee = new Employee("Carl Cracker", 80000, 1987, 12, 15);
+//        manager[0] = employee;
+//        manager[1] = new Manager("Harry Hacker", 50000, 1989, 10, 1);
+//        manager[2] = new Manager("Tony Tester", 40000, 1990, 3, 15);
+
+        /*
+         * 相反Employee的内存空间比Manager的小
+         * 反而可以存放
+         */
         Employee[] staff = new Employee[3];
-        staff[0] = new Employee("Carl Cracker", 75000, 1987, 12, 15);
+        Manager boss = new Manager("Carl Cracker", 80000, 1987, 12, 15);
+        staff[0] = boss;
         staff[1] = new Employee("Harry Hacker", 50000, 1989, 10, 1);
         staff[2] = new Employee("Tony Tester", 40000, 1990, 3, 15);
+
+//        ///////////////////////////错误的复制，扰乱了存储空间///////////////////////////
+//        Manager[] managers = new Manager[1];
+//        managers[0] = new Manager("Carl Cracker2", 80000, 1987, 12, 15);
+//        Employee[] staff2 = managers;
+//        staff2[0] = new Employee();
+//        /////////////////////////////////////////////////////////////////////////////////
+
+        boss.setBonus(5000);                    //ok
+        ((Manager)staff[0]).setBonus(5000);     //ok
+//        staff[0].setBonus(5000);                //worry
+//        ((Manager)staff[1]).setBonus(5000);     //worry
 
         for (Employee e : staff)
             e.raiseSalary(5);
@@ -14,6 +41,9 @@ public class Main {
         for (Employee e : staff)
             System.out.println("id=" + e.getId() + ", name=" + e.getName() + ", salary=" + e.getSalary() + ", hireDay=" + e.getHireDay());
 
+        for(Employee e:staff){
+            System.out.println(e.getName()+":"+e.getSalary());
+        }
 
     }
 }
--- Employee/src/com/company/Manager.java
+++ Employee/src/com/company/Manager.java
@@ -2,6 +2,11 @@ package com.company;
 
 public class Manager extends Employee {
     private double bonus;
+
+    public Manager(String name, double salary, int year, int month, int day){
+        super(name, salary, year, month, day);
+        bonus = 0;
+    }
     public void setBonus(double bonus){
         this.bonus = bonus;
     }
