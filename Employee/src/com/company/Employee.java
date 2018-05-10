1a7dd1d4ab7ce3400d4e6ad83a174ccad7721841
AAT.Admin.Colin <wuxinxin@zpmc.net>
2018-05-10 18:56:33 +0800

    Java核心技术 第4章

--- Employee/src/com/company/Employee.java
+++ Employee/src/com/company/Employee.java
@@ -1,10 +1,28 @@
 package com.company;
 
 import java.time.LocalDate;
+import java.util.Random;
 
 public class Employee {
-    private String name;
-    private double salary;
+    private static int nextId;
+//    private int id = assignId();
+    private int id;
+    private String name = "";
+    private double salary = 0.0;
+    private LocalDate hireDay = LocalDate.now();
+
+//    private static int assignId(){
+//        return nextId++;
+//    }
+
+    static{
+        Random generator = new Random();
+        nextId = generator.nextInt(100);
+    }
+
+    {
+        id = nextId++;
+    }
 
     public Employee() {
         name = "";
@@ -16,6 +34,13 @@ public class Employee {
         salary = s;
     }
 
+    public Employee(String n, double s, int year, int month, int day){
+        this(n,s);
+        hireDay = LocalDate.of(year, month, day);
+    }
+
+    public int getId(){return id;}
+
     public String getName() {
         return name;
     }
@@ -24,8 +49,17 @@ public class Employee {
         return salary;
     }
 
+    public LocalDate getHireDay(){
+        return hireDay;
+    }
+
     public void raiseSalary(double byPercent) {
         double raise = salary * byPercent / 100;
         salary += raise;
+    }
+
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("bye");
     }
 }
